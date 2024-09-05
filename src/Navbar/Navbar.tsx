63c2ffd3b0ffa29d3bbd85cf4139d45424887df4
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-05 22:27:58 +0300

    Add Navbar

---
+++ src/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import { Box, Image, Heading, Flex, Link } from "rebass";
+import { linkStyle, navbarStyle } from "../styles/styles";
+
+function NavBar() {
+  return (
+    <Box>
+      <Flex css={navbarStyle}>
+        <Flex alignItems="center">
+          <Image src="https://via.placeholder.com/50" alt="Musically Logo" />
+          <Heading fontSize={4} ml={2}>
+            Musically
+          </Heading>
+        </Flex>
+
+        <Flex alignItems="center">
+          <Link href="#songs" css={linkStyle}>
+            Songs
+          </Link>
+          <Link href="#artists" css={linkStyle}>
+            Artists
+          </Link>
+          <Link href="#albums" css={linkStyle}>
+            Albums
+          </Link>
+          <Link href="#genres" css={linkStyle}>
+            Genres
+          </Link>
+          <Link href="#genres" css={linkStyle}>
+            Statistics
+          </Link>
+        </Flex>
+      </Flex>
+    </Box>
+  );
+}
+
+export default NavBar;

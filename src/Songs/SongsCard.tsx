e5567bf1b541d7fafa5a75cb72ce762e29b496ab
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-05 22:27:33 +0300

    Add songs card

---
+++ src/Songs/SongsCard.tsx
@@ -0,0 +1,51 @@
+import { RootState } from "../store/configureStore";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchSongsRequest } from "./songsSlice";
+import { Box, Button, Card, Flex, Heading, Image, Text } from "rebass";
+import {
+  buttonStyle,
+  cardStyle,
+  contentStyle,
+  imageStyle,
+  sectionStyle,
+} from "../styles/styles";
+
+function SongsCard() {
+  const dispatch = useDispatch();
+  const { error, loading, songs } = useSelector(
+    (state: RootState) => state.songs
+  );
+
+  useEffect(() => {
+    // Dispatch fetch songs action on component mount
+    dispatch(fetchSongsRequest());
+  }, [dispatch]);
+
+  return (
+    <Box>
+      {/* Songs Section */}
+      <Box css={sectionStyle} id="songs">
+        <Heading fontSize={4}>Featured Songs</Heading>
+        <Flex css={contentStyle}>
+          {loading ? (
+            <Text>Loading...</Text>
+          ) : error ? (
+            <Text>Error: {error}</Text>
+          ) : (
+            songs.map((song) => (
+              <Card key={song._id} css={cardStyle}>
+                <Image src={song.imageUrl} css={imageStyle} alt={song.title} />
+                <Text fontWeight="bold">{song.title}</Text>
+                <Text>{song.artist}</Text>
+              </Card>
+            ))
+          )}
+        </Flex>
+        <Button css={buttonStyle}>View More Songs</Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default SongsCard;

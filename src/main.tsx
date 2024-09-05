3ce74430794c5f8da2ec32336944451e80d61aba
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-05 22:29:50 +0300

    Add theme and store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import theme from "./theme/theme.ts";
+import { Provider } from "react-redux";
+import { store } from "./store/configureStore";
+// import './index.css'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </StrictMode>
+);

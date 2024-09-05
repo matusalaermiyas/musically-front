b596f69ea05433fcfe3f79f964d29778cd4bd0fe
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-05 22:30:55 +0300

    Add card

---
+++ src/Artists/ArtistsCard.tsx
@@ -0,0 +1,55 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../store/configureStore";
+import { fetchArtistsRequest } from "./artistsSlice";
+import { Box, Button, Card, Flex, Heading, Image, Text } from "rebass";
+import {
+  buttonStyle,
+  cardStyle,
+  contentStyle,
+  imageStyle,
+  sectionStyle,
+} from "../styles/styles";
+import { useEffect } from "react";
+
+function ArtistsCard() {
+  const dispatch = useDispatch();
+  const { artists, loading, error } = useSelector(
+    (state: RootState) => state.artists
+  );
+
+  useEffect(() => {
+    // Dispatch fetch artists action on component mount
+    dispatch(fetchArtistsRequest());
+  }, [dispatch]);
+
+  return (
+    <Box>
+      {/* Artists Section */}
+      <Box css={sectionStyle} id="artists">
+        <Heading fontSize={4}>Top Artists</Heading>
+        <Flex css={contentStyle}>
+          {loading ? (
+            <Text>Loading...</Text>
+          ) : error ? (
+            <Text>Error: {error}</Text>
+          ) : (
+            artists.map((artist) => (
+              <Card key={artist._id} css={cardStyle}>
+                <Image
+                  src={artist.imageUrl}
+                  css={imageStyle}
+                  alt={artist.name}
+                />
+                <Text fontWeight="bold">{artist.name}</Text>
+                <Text>{artist.genre}</Text>
+              </Card>
+            ))
+          )}
+        </Flex>
+        <Button css={buttonStyle}>View More Artists</Button>
+      </Box>
+    </Box>
+  );
+}
+
+export default ArtistsCard;

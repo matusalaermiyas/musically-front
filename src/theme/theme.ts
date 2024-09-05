1ce6cc825e13c1dc54d41b6105c53b865e29b98a
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-05 22:26:09 +0300

    Add theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,37 @@
+// theme.ts
+import { Theme } from 'styled-system';
+
+const theme: Theme = {
+  space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+  fonts: {
+    body: 'system-ui, sans-serif',
+    heading: 'inherit',
+    monospace: 'Menlo, monospace',
+  },
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.125,
+  },
+  colors: {
+    text: '#000',
+    background: '#fff',
+    primary: '#07c',
+    secondary: '#30c',
+    muted: '#f6f6f6',
+  },
+  radii: {
+    default: 4,
+    circle: 99999,
+  },
+  shadows: {
+    card: '0 0 16px rgba(0, 0, 0, 0.25)',
+  },
+};
+
+export default theme;

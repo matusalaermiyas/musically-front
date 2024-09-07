28a9559a81737d622dfd889e906b75689df01846
MatusalaErmiyas <matusala.dev@gmail.com>
2024-09-07 23:41:48 +0300

    Add routing

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,31 @@ import { ThemeProvider } from "styled-components";
 import theme from "./theme/theme.ts";
 import { Provider } from "react-redux";
 import { store } from "./store/configureStore";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Statistics from "./Statistics/Statistics.tsx";
+import SongsPage from "./Songs/Songs.tsx";
 // import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/statistics",
+    element: <Statistics />,
+  },
+  {
+    path: "/songs",
+    element: <SongsPage />,
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
-        <App />
+        <RouterProvider router={router} />
       </Provider>
     </ThemeProvider>
   </StrictMode>
